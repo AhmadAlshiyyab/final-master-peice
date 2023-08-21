bc1435d0aa33c622b3f7e6b5b869987154d277d1
ahmad Alshiyyab <126424697+AhmadAlshiyyab@users.noreply.github.com>
2023-08-21 12:28:18 +0300

    final commit

---
+++ footer.php
@@ -0,0 +1,152 @@
+		<!-- Footer
+		============================================= -->
+		<footer id="footer" class="dark">
+
+			<div class="container">
+
+				<!-- Footer Widgets
+				============================================= -->
+				<div class="footer-widgets-wrap clearfix">
+
+					<div class="col_two_third">
+
+						<div class="col_one_third">
+
+							<div class="widget clearfix">
+
+								<img src="images/logo-footer.png" alt="" class="footer-logo">
+
+								<p>We believe in <strong>better Services</strong> &amp; <strong>Education</strong> is free and compulsory for all.</p>
+
+								<div style="background: url('images/world-map.png') no-repeat center center; background-size: 100%;">
+									<address>
+										<strong>Office:</strong><br>
+										Amman
+									</address>
+									<abbr title="Phone Number"><strong>Phone:</strong></abbr> +962772270389<br>
+									<abbr title="Phone Number"><strong>Phone:</strong></abbr> +962772270389<br>
+									<abbr title="Email Address"><strong>Email:</strong></abbr> ahmed.alshiyyab@gmail.com
+								</div>
+
+							</div>
+
+						</div>
+
+						
+
+						
+					</div>
+
+					<div class="col_one_third col_last">
+
+
+						
+							<h5><strong>Subscribe:</strong> Get in touch for upcoming <strong>Services</strong>, <strong>Technologies</strong>  &amp;<strong> Updates</strong>:</h5>
+							
+							<a href="contact.php"><button class="btn btn-success btn-lg btn-block" type="submit">Subscribe</button></a>
+									
+
+						<div class="widget clearfix" style="margin-bottom: -20px;">
+
+							<div class="row">
+
+								<div class="col-md-6 clearfix bottommargin-sm">
+									<a target="_blank" href="https://web.facebook.com/ahmed.alshyyab" class="social-icon si-dark si-colored si-facebook nobottommargin" style="margin-right: 10px;">
+										<i class="icon-facebook"></i>
+										<i class="icon-facebook"></i>
+									</a>
+									<a target="_blank" href="https://web.facebook.com/ahmed.alshyyab"><small style="display: block; margin-top: 3px;"><strong>Like us</strong><br>on Facebook</small></a>
+								</div>
+								<div class="col-md-6 clearfix">
+									<a href="https://twitter.com/alshyyab1996" class="social-icon si-dark si-colored si-twitter nobottommargin" style="margin-right: 10px;">
+										<i class="icon-twitter"></i>
+										<i class="icon-twitter"></i>
+									</a>
+									<a href="#"><small style="display: block; margin-top: 3px;"><strong>Follow</strong><br>On Twitter</small></a>
+								</div>
+
+							</div>
+
+						</div>
+
+					</div>
+
+				</div><!-- .footer-widgets-wrap end -->
+
+			</div>
+
+			<!-- Copyrights
+			============================================= -->
+			<div id="copyrights">
+
+				<div class="container clearfix">
+
+					<div class="col_half">
+						Copyrights &copy; 2023 All Rights Reserved by | Ahmad Alshiyyab<br>
+						<div class="copyright-links"> <a href="index.php?page=privacy">Privacy Policy</a> </div>
+					</div>
+
+					<div class="col_half col_last tright">
+						<div class="fright clearfix">
+
+							<a target="_blank" href="https://web.facebook.com/ahmed.alshyyab" class="social-icon si-small si-borderless si-facebook">
+								<i class="icon-facebook"></i>
+								<i class="icon-facebook"></i>
+							</a>
+
+							<a target="_blank" href="https://twitter.com/alshyyab1996" class="social-icon si-small si-borderless si-twitter">
+								<i class="icon-twitter"></i>
+								<i class="icon-twitter"></i>
+							</a>
+
+							<a target="_blank" href="https://www.instagram.com/ahmedshyyab/" class="social-icon si-small si-borderless si-instagram">
+								<i class="icon-instagram"></i>
+								<i class="icon-instagram"></i>
+							</a>
+
+							<a target="_blank" href="https://www.pinterest.com/ahmed1996alshyyab/" class="social-icon si-small si-borderless si-pinterest">
+								<i class="icon-pinterest"></i>
+								<i class="icon-pinterest"></i>
+							</a>
+
+							<a target="_blank" href="https://www.linkedin.com/in/ahmad-alshiyyab-b02a65267/" class="social-icon si-small si-borderless si-dropbox">
+								<i class="icon-linkedin"></i>
+								<i class="icon-linkedin"></i>
+							</a>
+
+							<a target="_blank" href="https://github.com/AhmadAlshiyyab" class="social-icon si-small si-borderless si-github">
+								<i class="icon-github"></i>
+								<i class="icon-github"></i>
+							</a>
+
+							
+						</div>
+
+						<div class="clear"></div>
+
+						<i class="icon-envelope2"></i> ahmed.alshiyyab@gmail.com <span class="middot">&middot;</span> <i class="icon-headphones"></i> +962772270389 <span class="middot"></span>
+					</div>
+
+				</div> <!-- End -->
+
+			</div><!-- #copyrights end -->
+
+		</footer><!-- #footer end -->
+
+	</div><!-- #wrapper end -->
+
+	<!-- Go To Top
+	============================================= -->
+	<div id="gotoTop" class="icon-angle-up"></div>
+
+	<!-- External JavaScripts
+	============================================= -->
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/plugins.js"></script>
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script type="text/javascript" src="js/functions.js"></script>
+
+</body>
+</html>
